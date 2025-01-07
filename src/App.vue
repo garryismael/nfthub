be13433b841c9802ac25cb4f73fef9d5b39058a5
Garry <tahinjanaharygarry@gmail.com>
2025-01-07 19:35:53 +0300

    init project

---
+++ src/App.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="flex flex-col items-center justify-center h-screen">
+    <img src="/haha.png" alt="haha" class="max-w-32" />
+    <h1 class="text-lg font-medium">Mba asio m-rouler R moa.</h1>
+  </div>
+</template>
