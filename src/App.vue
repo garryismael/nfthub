0ab6c9e4196f162d73f4d1b0b1a6e2d1d474131c
Garry <tahinjanaharygarry@gmail.com>
2025-01-08 08:56:04 +0300

    add header nav

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
+<script setup>
+import HeaderNav from './components/HeaderNav.vue'
+</script>
 <template>
-  <div class="flex flex-col items-center justify-center h-screen">
+  <header-nav />
+  <div class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
     <img src="/haha.png" alt="haha" class="max-w-32" />
     <h1 class="text-lg font-medium">Mba asio m-rouler R moa.</h1>
   </div>
