04d5580a0756d0b7ab3e15c41367772cf978e09a
Garry <tahinjanaharygarry@gmail.com>
2025-01-09 09:08:01 +0300

    add hero section

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
 <script setup>
 import HeaderNav from './components/HeaderNav.vue'
+import HeroSection from './sections/HeroSection.vue'
 </script>
 <template>
   <header-nav />
-  <div class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-    <img src="/haha.png" alt="haha" class="max-w-32" />
-    <h1 class="text-lg font-medium">Mba asio m-rouler R moa.</h1>
-  </div>
+  <hero-section />
 </template>
