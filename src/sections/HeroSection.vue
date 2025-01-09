7e518333bca6666606b76427e9db76dec71d5fee
Garry <tahinjanaharygarry@gmail.com>
2025-01-09 09:08:17 +0300

    add hero section

---
+++ src/sections/HeroSection.vue
@@ -0,0 +1,40 @@
+<template>
+  <section class="grid grid-cols-1 lg:grid-cols-2 mt-[78px]">
+    <div class="text-center">
+      <h1>High Quality NFT Collection</h1>
+      <p>A 890 piece custom Nfthub's collection is joining the NFT space on Opensea.</p>
+      <button class="flex items-center gap-3 mx-auto rounded-full btn-gradient mt-9">
+        <span>View in OPENSEA</span
+        ><svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          viewBox="0 0 16 16"
+          fill="none"
+        >
+          <path
+            d="M3.55078 0.643311V2.14331H12.9961L0.550781 14.5886L1.60547 15.6433L14.0508 3.198V12.6433H15.5508V0.643311H3.55078Z"
+            fill="white"
+          />
+        </svg>
+      </button>
+    </div>
+    <div>
+      <img src="/hero.png" alt="Hero Image" width="651" height="639" />
+    </div>
+  </section>
+</template>
+
+<style scoped>
+h1 {
+  @apply text-[40px] leading-10 lg:leading-[72px] lg:text-7xl;
+  color: #0f051d;
+  font-weight: 700;
+}
+
+p {
+  @apply text-[19px] lg:text-[22px] leading-[30px] lg:leading-9 mt-6;
+  color: #7b7583;
+  font-weight: 400;
+}
+</style>
